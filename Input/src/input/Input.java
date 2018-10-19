3002b4a4cc9e0be21d0410f3f3f1b283941a248f
guerrjos <guerrjos@011-4303-S12QD.students.cisd.conroeisd.loc>
2018-10-19 08:10:24 -0500

    added a print statement to show my change.

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -36,6 +36,7 @@ public class Input {
           out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2], stringArr[3], stringArr[4]);
           
           System.out.println("\nI mada a change.");
+          System.out.println("you ugly");
           
           
           
