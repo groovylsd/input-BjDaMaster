cb88cf32d1f6b2daf6e6069018da75a17d1ee0f2
vawteaid <vawteaid@011-4303-S28ZJ.students.cisd.conroeisd.loc>
2018-10-19 07:49:43 -0500

    Added a print statement to show my change.

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -35,7 +35,7 @@ public class Input {
           
           out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2], stringArr[3], stringArr[4]);
           
-          
+          System.out.println("\nI mada a change.");
           
           
           
